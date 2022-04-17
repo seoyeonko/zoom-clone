e5cd16566a1779f1063adc23d96883475b2aa15a
Seoyeon Ko <55572222+seoyeonko@users.noreply.github.com>
2022-04-17 21:37:20 +0900

    #1.8 채팅 주고받기, 닉네임 적용하기

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,17 +1,42 @@
+const $messageList = document.querySelector('ul');
+const $messageForm = document.querySelector('#message');
+const $nickForm = document.querySelector('#nickname');
 const socket = new WebSocket(`ws://${window.location.host}`);
 
-socket.addEventListener('open', () => {
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+function handleOpen() {
   console.log('Connected to Server! ✅');
-});
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = $messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = $nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
+  input.value = '';
+}
+
+socket.addEventListener('open', handleOpen);
 
 socket.addEventListener('message', (message) => {
-  console.log('New message: ', message.data);
+  const $li = document.createElement('li');
+  $li.innerText = message.data;
+  $messageList.append($li);
 });
 
 socket.addEventListener('close', () => {
   console.log('Disconnected from Server! ❌');
 });
 
-setTimeout(() => {
-  socket.send('hello from the browser!');
-}, 10000);
+$messageForm.addEventListener('submit', handleSubmit);
+$nickForm.addEventListener('submit', handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -15,13 +15,33 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server }); // http server 위에 web socket server
 // 동일한 port에서 http, ws req 처리 가능
 
+function onSocketClose() {
+  console.log('Disconnected from the Browser! ❌');
+}
+
+const sockets = []; // 접속자
+
 wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anon'; // default nickname
   console.log('Connected to Browser! ✅');
-  socket.on('close', () => console.log('Disconnected from the Browser! ❌'));
-  socket.on('message', (message) => {
-    console.log(message);
+  socket.on('close', onSocketClose);
+  socket.on('message', (msg) => {
+    // user가 보낸 msg; user에게 보여주기
+
+    // JSON.parse(): string -> javascript Object
+    const message = JSON.parse(msg.toString());
+
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket.nickname} : ${message.payload}`);
+        });
+
+      case 'nickname':
+        socket['nickname'] = message.payload; // socket은 object이므로 new 정보 저장 가능
+    }
   });
-  socket.send('hello');
 });
 
 server.listen(3000, handleListen);
